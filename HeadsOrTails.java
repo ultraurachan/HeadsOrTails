ec5f9f12c3cfd3b78185a314e3fe814fc52ea595
ultraurachan <lapis0728@gmail.com>
2024-06-28 12:47:39 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+
+        Random random = new Random();
+        int headsCount = 0;
+        int tailsCount = 0;
+
+        System.out.println("Tossing a coin...");
+        for (int i = 1; i <= 3; i++) {
+            String result = random.nextBoolean() ? "Heads" : "Tails";
+            if (result.equals("Heads")) {
+                headsCount++;
+            } else {
+                tailsCount++;
+            }
+            System.out.println("Round " + i + ": " + result);
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# HeadsOrTails
+# HeadsOrTails
